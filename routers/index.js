d937521754d5c2d9bd557302f9506c40a92dcbda
Egor Bokov <s1lice.egor@gmail.com>
2022-11-24 23:59:13 +0300

    добавлено отправление почты пользователю

--- routers/index.js
+++ routers/index.js
@@ -1,11 +1,13 @@
 const Router = require('express')
 const router = new Router()
+const nodemailer = require('nodemailer')
 const db = require('../db')
 const medicalRouter = require('./medicalRouter')
 const clothesRouter = require('./clothesRouter')
 const shieldsRouter = require('./shieldsRouter')
 const backpacksRouter = require('./backpacksRouter')
 const guardsRouter = require('./guardsRouter')
+const {getLogger} = require("nodemailer/lib/shared");
 
 router.get('/categories', async (req, res) => {
     const links = await db.query('SELECT * FROM categories;')
@@ -17,4 +19,34 @@ router.use('/clothes', clothesRouter)
 router.use('/shields', shieldsRouter)
 router.use('/backpacks', backpacksRouter)
 router.use('/guards', guardsRouter)
+router.post('/create-order', createPurchase)
+
+function createPurchase(req, res) {
+    const { phone, email, country, city, name } = req.body
+
+    const transport = nodemailer.createTransport({
+        host: 'smtp.gmail.com',
+        port: 465,
+        secure: true,
+        auth: {
+            user: 's1lice.egor@gmail.com',
+            pass: 'ljtpzjempymnqlwf'
+        }
+    })
+
+    const message = {
+        from: 's1lice.egor@gmail.com',
+        to: email,
+        subject: `Заказ в ${country}, ${city}`,
+        text: `${name} ${email} ${phone} ${country} ${city}`
+    }
+
+    transport.sendMail(message, (err) => {
+        if (err) {
+            return console.log(err)
+        }
+        res.json({message: 'Сообщение было отправлено успешно!'})
+    })
+}
+
 module.exports = router
